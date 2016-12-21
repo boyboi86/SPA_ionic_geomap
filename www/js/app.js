4e185b012599017223935fdfae549fd18ffff58f
boyboi86 <boyboi86@gmail.com>
2016-12-22 01:04:27 +0800

    update music files/ app.js

--- www/js/app.js
+++ www/js/app.js
@@ -15,14 +15,16 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
 })
 
-app.controller('MapCtrl', function($scope, $state, $interval, $window, $cordovaGeolocation, $cordovaVibration, $cordovaMedia) {
+app.controller('MapCtrl', function($scope, $state, $window, $cordovaGeolocation, $cordovaVibration, $cordovaDialogs) {
 $scope.map = null;
 $scope.marker = [];
 $scope.locationMarker = [];
+$scope.intervals = [];
+$window.localStorage.clear();
+$window.localStorage.setItem('navigationState', angular.toJson({"state": false, "toggle": false}))
 //Initialize Geolocation fn
 googleMap();
-// watchPosition is not feasible for testing, so use intervals to keep track
-// $interval(geoLoc, 40000 );
+// watchPosition is not feasible for testing, so use timeout to keep track
 
 
 
@@ -127,7 +129,7 @@ function googleMap(){
           directionsDisplay.setDirections(response);
           showSteps(response, $scope.marker, stepDisplay, $scope.map, fromLocation, toLocation);
         } else {
-          $window.alert('Directions request failed due to ' + status);
+          $cordovaDialogs.alert('Directions request failed due to ' + status, "Try again!");
         }
       });
     }
@@ -149,7 +151,15 @@ function googleMap(){
         attachInstructionText(stepDisplay, marker, myRoute.steps[i].instructions, $scope.map);
         console.log(Arr);
       }
-      $window.localStorage.setItem('recentSearch', angular.toJson({from: fromLocation, to: toLocation, marker: Arr}));
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode({'address': toLocation}, function(results, status) {
+        console.log(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+        var toLocObj = {"lat": results[0].geometry.location.lat(), "lng": results[0].geometry.location.lng()}
+        Arr.push(toLocObj);
+
+        $window.localStorage.setItem('recentSearch', angular.toJson({from: fromLocation, to: toLocation, marker: Arr}));
+      });
+      // $window.localStorage.setItem('recentSearch', angular.toJson({from: fromLocation, to: toLocation, marker: Arr}));
     }
 
     function attachInstructionText(stepDisplay, marker, text, map) {
@@ -161,7 +171,7 @@ function googleMap(){
       });
     }
   }
-
+// trigger this function to have update on existing current position + any previous routes
 $scope.findMeHandler = function(){
     var options = {timeout: 10000, enableHighAccuracy: true};
 
@@ -171,20 +181,104 @@ $scope.findMeHandler = function(){
 
     var mapOptions = {
       center: latLng,
-      zoom: 14,
+      zoom: 13,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    var recentSearch = angular.fromJson($window.localStorage.getItem('recentSearch'))
-    $scope.AutocompleteDirectionsHandler(recentSearch.from, recentSearch.to);
+    var recentSearch = angular.fromJson($window.localStorage.getItem('recentSearch'));
+    if(recentSearch){
+      $scope.AutocompleteDirectionsHandler(recentSearch.from, recentSearch.to);
+    }
     geoMapping(latLng, position.coords.latitude, position.coords.longitude);
+    changeToggleState()
   }, function(error){
     console.log("Could not get location");
   });
 }
+// Calculate distance between 2 markers/ geolocation
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2, index, totalIndex) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+  console.log(d);
+  if(ionic.Platform.isAndroid() === true && d <= 0.3){
+    // if you are using chrome, if device set to android this will become a valid arguement
+    nativeDeviceAlarm(index, d);
+  }
+  if(d <= 0.3){
+    index += 1;
+    $cordovaDialogs.alert("You are " + d.toFixed(2) + " Km" + " away" + " from " + index + " of " + totalIndex + " check point!");
+  }
+  return d;
+}
 
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
 
+function nativeDeviceAlarm(index, dist){
+  // feature only available in real device
+  index += 1;
+  $cordovaDialogs.beep(index);
+  $cordovaDialogs.alert("You are " + dist.toFixed(2) + " Km" + " away", index + " of " + totalIndex + " check point!");
+  $cordovaVibration.vibrate(2000);
+}
 
+// for checking coordinates distance loop fn
+function checkDistbetweenMarkers(currentLat, currentLng){
+  var recentSearch = angular.fromJson($window.localStorage.getItem('recentSearch'));
+  var Arr = recentSearch.marker
+  for(var i = 0; i < Arr.length; i++){
+    var lat = Arr[i].lat;
+    var lng = Arr[i].lng;
+    getDistanceFromLatLonInKm(currentLat, currentLng, lat, lng, i, Arr.length);
+  }
+}
+// GPS interval tracking to ensure users are notified once they are close by fn
+function toggleNavigationTracking(){
+  var options = {maximumAge: 0, timeout: 30000, enableHighAccuracy: true};
+  $cordovaGeolocation.getCurrentPosition(options)
+    .then(function(position){
+    checkDistbetweenMarkers(position.coords.latitude, position.coords.longitude);
+    $scope.startInterval();
+  })
+  .catch(function(error){
+    console.log("Navigation tracking not enabled!", error);
+  })
+  console.log("toggleNavigationTracking intialized")
+}
+
+// trigger to have loop fn for constant GPS update
+function changeToggleState(){
+  var navigationState = $window.localStorage.getItem('navigationState');
+  var localStore = angular.fromJson(navigationState);
+  var state = localStore.state
+  var toggle = localStore.toggle
+  $scope.startInterval = function(){
+    $scope.toggleInt = $window.setTimeout(toggleNavigationTracking, 10000);
+  }
+
+  $scope.stopInterval = function(){
+    $window.clearTimeout($scope.toggleInt);
+  }
+
+  if(state == true && toggle == true || state == 'true' && toggle == 'true'){
+    $window.localStorage.setItem('navigationState', angular.toJson({"state": false, "toggle": false}));
+    $scope.stopInterval()
+  }
+
+  if(state == false && toggle == false || state == 'false' && toggle == 'false'){
+    $window.localStorage.setItem('navigationState', angular.toJson({"state": true, "toggle": true}));
+    $scope.startInterval()
+  }
+}
 
 
 
