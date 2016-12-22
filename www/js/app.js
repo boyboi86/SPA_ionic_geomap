fc59c528e6a63c7f3157746c885696e07123f438
boyboi86 <boyboi86@gmail.com>
2016-12-22 11:07:11 +0800

    final push

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,6 @@
 (function() {
 
-var app = angular.module('starter', ['ionic', 'ngCordova'])
+var app = angular.module('starter', ['ionic', 'ngCordova', 'firebase'])
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
@@ -9,9 +9,14 @@ app.config(function($stateProvider, $urlRouterProvider) {
     url: '/',
     templateUrl: 'templates/map.html',
     controller: 'MapCtrl'
-  });
+  })
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'MapCtrl'
+  })
 
-  $urlRouterProvider.otherwise("/");
+  $urlRouterProvider.otherwise("/login");
 
 })
 
@@ -20,7 +25,7 @@ $scope.map = null;
 $scope.marker = [];
 $scope.locationMarker = [];
 $scope.intervals = [];
-$window.localStorage.clear();
+$window.localStorage.removeItem('recentSearch');
 $window.localStorage.setItem('navigationState', angular.toJson({"state": false, "toggle": false}))
 
 //Initialize Geolocation fn
@@ -55,7 +60,7 @@ var geoMapping = function(latLng, latitude, longitude){
 
     // // Info Window to display current location
     var currentPositionInfoWindow = new google.maps.InfoWindow({
-        content: "Your current location is at " + latLng + " !"
+        content: "You are here!!"
     });
     // For random clicking for the map, includes a marker
     google.maps.event.addListener(marker, 'click', function() {
@@ -305,7 +310,82 @@ $scope.changeToggleState = function(){
   }
 }
 
+$scope.fave1 = function(){
+  var win = $window.localStorage
+  if(win.getItem('recentSearch') && !win.getItem('fave1')){
+    win.setItem('fave1', win.getItem('recentSearch'));
+  }
+    win.setItem('recentSearch', win.getItem('fave1'))
+    $scope.findMeHandler();
+}
 
+$scope.fave2 = function(){
+  var win = $window.localStorage
+  if(win.getItem('recentSearch') && !win.getItem('fave2')){
+    win.setItem('fave2', win.getItem('recentSearch'));
+  }
+    win.setItem('recentSearch', win.getItem('fave2'))
+    $scope.findMeHandler();
+}
+
+
+
+
+// Social Authentication: facebook;
+$scope.login = function(){
+var provider = new firebase.auth.FacebookAuthProvider();
+
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
+firebase.auth().signInWithRedirect(provider);
+  fbRedirectWithToken();
+}
+
+function fbRedirectWithToken(){
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+  }
+  // The signed-in user info.
+  var user = result.user;
+  $state.go('map');
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+}
+
+$scope.logout = function(){
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    $state.go('login');
+  }, function(error) {
+    // An error happened.
+    console.log('logout error: ', error);
+  });
+}
 
 });
 
